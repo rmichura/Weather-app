85f5425ae01328fb40da783647299ab13c7b940d
rmichura <r.michura@wp.pl>
2022-06-02 15:33:10 +0200

    Added new path for weather

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import WeatherView from '../views/WeatherView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,9 @@ const routes: Array<RouteConfig> = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/weather',
+    name: 'weather',
+    component: WeatherView
   }
 ]
 
