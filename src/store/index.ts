56a13ac5420693c0467307f5076a29ccf9bbd1bd
rmichura <r.michura@wp.pl>
2022-06-02 15:35:11 +0200

    Added arrays to store in vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cityList from "../../city.json";
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cityList: cityList,
+    myCity: [],
+    cityFromApi: [],
+    lat: '',
+    lon: '',
   },
   getters: {
+    getCityList(state) {
+      return state.cityList
+    },
+    getMyCity(state) {
+      return state.myCity
+    },
+    getCityFromApi(state) {
+      return state.cityFromApi
+    }
   },
   mutations: {
   },
   actions: {
+    // currentCity: async function ({state}) {
+    //   try {
+    //     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${state.lat}&lon=${state.lon}&appid=555e67159798f21c4b0a6c81f18ad428&units=metric`)
+    //     state.cityFromApi.push(response as any)
+    //   } catch (e) {
+    //     console.log(e)
+    //   }
+    // }
   },
   modules: {
   }
